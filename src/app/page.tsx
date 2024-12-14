3412a2caaf51ced9fafb0d87ae0c3e22e1276fef
Ash <68016221+AshOnDiscord@users.noreply.github.com>
2024-12-14 14:21:14 -0500

    Working Selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,5 +5,9 @@ import React from "react";
 export default function Home() {
   const [month, setMonth] = React.useState(new Date().getMonth());
 
-  return <MonthlyCalendar month={month} setMonth={setMonth} />;
+  return (
+    <main className="flex flex-col items-center justify-center gap-4 p-4 min-h-screen bg-slate-100">
+      <MonthlyCalendar month={month} setMonth={setMonth} />
+    </main>
+  );
 }
--- src/components/monthlyCalendar.tsx
+++ src/components/monthlyCalendar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 export default function MonthlyCalendar({
   month,
@@ -7,7 +8,7 @@ export default function MonthlyCalendar({
   month: number;
   setMonth: React.Dispatch<React.SetStateAction<number>>;
 }) {
-  // const month = new Date().getMonth();
+  const [active, setActive] = useState<number | null>(null);
 
   // val day of the week the month starts on
   const dayOfWeek = new Date(2024, month, 1).getDay();
@@ -32,49 +33,66 @@ export default function MonthlyCalendar({
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th colSpan={7}>
-            <div className="flex justify-center">
-              <h2 className="text-center">
-                {new Date(2024, month, 1).toLocaleDateString("en-US", {
-                  month: "long",
-                  year: "numeric",
-                })}
-              </h2>
-              <button onClick={() => setMonth(month - 1)}>{"<"}</button>
-              <button onClick={() => setMonth(month + 1)}>{">"}</button>
-            </div>
-          </th>
-        </tr>
-        <tr>
-          <th>S</th>
-          <th>M</th>
-          <th>T</th>
-          <th>W</th>
-          <th>T</th>
-          <th>F</th>
-          <th>S</th>
-        </tr>
-      </thead>
-      <tbody className="text-center">
-        {Array.from({ length: 6 }, (_, i) => (
-          <tr key={i}>
-            {Array.from({ length: 7 }, (_, j) => {
-              const { number, current } = getDay(i, j);
-              return (
-                <td
-                  key={`${i}-${j}`}
-                  className={current ? "text-slate-900" : "text-slate-300"}
-                >
-                  {number}
-                </td>
-              );
-            })}
+    <div className="bg-white rounded-xl border border-slate-200 overflow-hidden p-4">
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={7}>
+              <div className="flex justify-between">
+                <h2 className="text-center">
+                  {new Date(2024, month, 1).toLocaleDateString("en-US", {
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </h2>
+                <div className="flex gap-2">
+                  <button onClick={() => setMonth(month - 1)}>{"<"}</button>
+                  <button onClick={() => setMonth(month + 1)}>{">"}</button>
+                </div>
+              </div>
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+          <tr>
+            <th>S</th>
+            <th>M</th>
+            <th>T</th>
+            <th>W</th>
+            <th>T</th>
+            <th>F</th>
+            <th>S</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {Array.from({ length: 6 }, (_, i) => (
+            <tr key={i}>
+              {Array.from({ length: 7 }, (_, j) => {
+                const { number, current } = getDay(i, j);
+                const isActive = month * 31 * 31 + i * 31 + j === active;
+                return (
+                  <td key={`${i}-${j}`}>
+                    <button
+                      className={[
+                        current
+                          ? isActive
+                            ? "text-amber-900"
+                            : "text-slate-900"
+                          : isActive
+                          ? "text-amber-600"
+                          : "text-slate-300",
+                        isActive ? "bg-amber-300 rounded-full" : "",
+                        "px-2 py-1 w-full aspect-square",
+                      ].join(" ")}
+                      onClick={() => setActive(month * 31 * 31 + i * 31 + j)}
+                    >
+                      {number}
+                    </button>
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
